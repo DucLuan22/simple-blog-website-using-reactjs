636e34c5d21195536b4d2e3836e5f3b5866d3417
DucLuan22 <ducluandang22@gmail.com>
2024-06-14 22:06:56 +0700

    fixed bugs

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -13,75 +13,72 @@ const Sidebar = ({ children }: SidebarProps) => {
   };
 
   return (
-    <div>
-      <div className="flex h-screen">
-        <div
-          className={`fixed inset-0 z-50 transition-transform transform ${
-            isOpen ? "translate-x-0" : "-translate-x-full"
-          } bg-gray-800 text-white w-64 h-full lg:translate-x-0 lg:static lg:w-64`}
-        >
-          <div className="p-4 mt-8 lg:mt-0">
-            <nav className="mt-6">
-              <ul>
-                <li>
-                  <Link
-                    to="/admin"
-                    className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700"
-                  >
-                    Admin Dashboard
-                  </Link>
-                </li>
-
-                <li>
-                  <Link
-                    to="/profile"
-                    className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700"
-                  >
-                    Profile
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/post-history"
-                    className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700"
-                  >
-                    Post History
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/"
-                    className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700"
-                  >
-                    Return Home
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
+    <div className="flex min-h-screen">
+      <div
+        className={`fixed inset-y-0 z-50 transition-transform transform ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } bg-gray-800 text-white w-64 lg:translate-x-0 lg:static lg:w-64`}
+      >
+        <div className="p-4 mt-8 lg:mt-0">
+          <nav className="mt-6">
+            <ul>
+              <li>
+                <Link
+                  to="/admin"
+                  className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700"
+                >
+                  Admin Dashboard
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/profile"
+                  className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700"
+                >
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/post-history"
+                  className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700"
+                >
+                  Post History
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/"
+                  className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700"
+                >
+                  Return Home
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <div className="flex-1 flex flex-col">
-          <header className="z-50 flex items-center justify-between p-4 bg-gray-800 text-white lg:hidden">
-            <button onClick={toggleSidebar} className="focus:outline-none">
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
-                ></path>
-              </svg>
-            </button>
-            <h1 className="text-xl font-bold">luanblog</h1>
-          </header>
-          <main className="flex-1 p-4">{children}</main>
-        </div>
+      </div>
+      <div className="flex-1 flex flex-col">
+        <header className="z-50 flex items-center justify-between p-4 bg-gray-800 text-white lg:hidden">
+          <button onClick={toggleSidebar} className="focus:outline-none">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
+              ></path>
+            </svg>
+          </button>
+          <h1 className="text-xl font-bold">luanblog</h1>
+        </header>
+        <main className="flex-1 p-4 overflow-y-auto">{children}</main>
       </div>
     </div>
   );
--- src/components/adminpage/Body.tsx
+++ src/components/adminpage/Body.tsx
@@ -5,16 +5,17 @@ import LineGraph from "./LineGraph";
 function Body() {
   return (
     <div className="flex gap-10 flex-col lg:flex-row">
-      <div className="w-full h-[500px] border-[2px] lg:basis-2/3">
+      <div className="w-full lg:basis-2/3">
+        <LineGraph />
         <LineGraph />
       </div>
-      <div className="border-[1px] w-full lg:basis-1/3 rounded-xl bg-white">
+      <div className="border-[1px] h-[500px] w-full lg:basis-1/3 rounded-xl bg-white">
         <div className="p-4 flex gap-2 items-center bg-gray-300 rounded-tl-xl rounded-tr-xl">
           <Bell className="w-6 h-6" />
           <h1>Notifications Panel</h1>
         </div>
 
-        <div className="m-5 border-[1px] border-gray-300 h-[500px] flex flex-col">
+        <div className="m-5 border-[1px] border-gray-300 flex flex-col">
           <div className="flex p-4 justify-between border-b-[1px] border-gray-300">
             <div className="flex gap-2 items-center">
               <MessageCircle className="w-6 h-6" />
