89b4ace884249a6f80542e2d3e5dca8f321ce7ee
DucLuan22 <ducluandang22@gmail.com>
2024-10-03 23:06:34 +0700

    added code editor modal

--- src/components/adminpage/PostEditorModal.tsx
+++ src/components/adminpage/PostEditorModal.tsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "../ui/dialog";
 import { Button } from "../ui/button";
 import { Edit } from "lucide-react";
+import ReactQuill from "react-quill";
 
 function PostEditorModal() {
+  const reactQuillRef = useRef<ReactQuill>(null);
   return (
     <Dialog>
       <DialogTrigger>
@@ -18,14 +19,66 @@ function PostEditorModal() {
           <Edit className="w-4 h-4" />
         </Button>
       </DialogTrigger>
-      <DialogContent className="w-full max-w-[90%] h-[90%]">
+      <DialogContent className="w-full max-w-[90%] max-h-[90%]">
         <DialogHeader>
           <DialogTitle>Post Editor</DialogTitle>
-          <div className="">
-            <div></div>
-            <div></div>
-          </div>
         </DialogHeader>
+        <div className="w-full h-auto flex flex-col lg:flex-row">
+          {/* Left Side - Responsive width */}
+          <div className="w-full lg:w-2/3">
+            <ReactQuill
+              theme="snow"
+              ref={reactQuillRef}
+              placeholder="Tell your story..."
+              modules={{
+                toolbar: {
+                  container: [
+                    [{ header: "1" }, { header: "2" }, { font: [] }],
+                    [{ size: [] }],
+                    ["bold", "italic", "underline", "strike", "blockquote"],
+                    [
+                      { list: "ordered" },
+                      { list: "bullet" },
+                      { indent: "-1" },
+                      { indent: "+1" },
+                    ],
+                    ["link", "image", "video"],
+                    ["code-block"],
+                    ["clean"],
+                  ],
+                },
+                clipboard: {
+                  matchVisual: false,
+                },
+              }}
+              formats={[
+                "header",
+                "font",
+                "size",
+                "bold",
+                "italic",
+                "underline",
+                "strike",
+                "blockquote",
+                "list",
+                "bullet",
+                "indent",
+                "link",
+                "image",
+                "video",
+                "code-block",
+              ]}
+              value={""}
+              className="w-full text-foreground h-[400px] lg:h-[600px] mb-10"
+            />
+          </div>
+
+          {/* Right Side - Responsive width */}
+          <div className="w-full lg:w-1/3 p-4 mt-6 lg:mt-0">
+            <h2 className="text-lg font-semibold">Right Side Content</h2>
+            <p>This is the right side of the editor.</p>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
